de528e70dfe103c736ff7853c3204ad7bf0f0ec6
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-21 13:56:28 +0000

    Final solution to re-writing inject challenge

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,33 +1,61 @@
 class Array 
 
-# can add and subtract numbers together
-  # def new_inject(*args, &block)
-  #     copy = self.dup
-  #     memo = copy.shift
-  #      x = eval copy.join '+'
-  #       yield(memo, x) if block_given? 
+# # can add numbers together
+  def new_inject(*args, &block)
+      copy = self.dup
+      
+      if args[0].is_a? Symbol
+        method = args.shift.to_s
+        sum = eval copy.join method
+      elsif args[0].is_a? Integer 
+        if block_given?
+          sum = args[0]
+          while copy.length > 0
+            next_item = copy.shift
+            sum = yield(sum, next_item) if block_given?
+          end
+        else 
+          start = args.shift
+          method = args.shift.to_s
+          sum = start + (eval copy.join method)
+        end
+      else args == nil
+        first_item = copy.shift
+        second_item = copy.shift
+        sum = yield(first_item, second_item) if block_given?  
+        while copy.length > 0
+          next_item = copy.shift
+          sum = yield(sum, next_item) if block_given?
+        end
+      end
+    sum
+end
+
+  # def new_inject(*args, &block) 
+  #   copy = self.dup
+  #   while copy.length > 0 
+  #     if copy.length == self.length
+  #       x = copy.shift
+  #       p "I am x:#{x}"
+  #       y = copy.shift
+  #       p "I am y:#{y}"
+  #     else
+  #       y = copy.shift
+  #       p "I am y shift: #{y}"
+  #     end
+  #     x = yield(x,y) if block_given?
+  #     p "I am x final: #{x}"
+  #   end
+  #   x
   # end
 
 
-# # can add and multiply numbers together
-#   def new_inject(*args, &block)
-#       copy = self.dup
-#       memo = copy.pop
-#        x = eval copy.join '+'
-#         yield(memo, x) if block_given? 
-#   end
+# def new_inject
+
+
 # end
 
-# can add and multiply numbers together
-  def new_inject(*args, &block)
-      copy = self.dup
-      sum = 0
-      while copy.length > 0
-      x = copy.shift
-      y = copy.shift
-      sum += yield(x, y) if block_given? 
-    end
-    sum
-    p sum
-  end
+
 end
+
+
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -7,6 +7,7 @@ describe Array do
     it 'can add numbers together' do 
       expect([1,2,3,4].inject{|memo,item| memo+item}).to eq(10)
       expect([1,2,3,4].new_inject{|memo, item| memo + item}).to eq(10)
+      expect([3,5,8,9].new_inject{|memo, item| memo + item}).to eq(25)
     end
 
     it 'can minus numbers' do
@@ -21,42 +22,47 @@ describe Array do
 
   end
 
-  #   context 'with a symbol' do
+    context 'with a symbol' do
 
-  #     it 'can add numbers together' do
-  #       expect([1,2,3,4].inject(:+)).to eq(10)
-  #       expect([1,2,3,4].new_inject(:+)).to eq(10)
-  #     end
+      it 'can add numbers together using a symbol' do
+        expect([1,2,3,4].inject(:+)).to eq(10)
+        expect([1,2,3,4].new_inject(:+)).to eq(10)
+      end
 
-  #     it 'can subtract numbers from each other' do
-  #       expect([1,2,3,4].inject(:-)).to eq(-8)
-  #       expect([1,2,3,4].new_inject(:-)).to eq(-8)
-  #    end
+      it 'can subtract numbers from each other using a symbol' do
+        expect([1,2,3,4].inject(:-)).to eq(-8)
+        expect([1,2,3,4].new_inject(:-)).to eq(-8)
+     end
   
-  # end  
+  end  
 
-  #   context 'with an argument and a block' do
+    context 'with an argument and a block' do
     
-  #     it 'it can add with a starting point' do
-  #       expect([1,2,3,4].inject(10) {|memo, item| memo + item}).to eq(20)
-  #       expect([1,2,3,4].new_inject(10) {|memo, item| memo + item}).to eq(20)
+      it 'it can add with a starting point and a block' do
+        expect([1,2,3,4].inject(10) {|memo, item| memo + item}).to eq(20)
+        expect([1,2,3,4].new_inject(10) {|memo, item| memo + item}).to eq(20)
+      end
    
-  #     end
-  #    it 'can subtract with a starting point' do
-  #       expect([1,2,3,4].inject(:-)).to eq(-8)
-  #       expect([1,2,3,4].new_inject(:-)).to eq(-8)
-  #    end
+      it 'it can multiply with a starting point and a block' do
+        expect([1,2,3,4].inject(10) {|memo, item| memo * item}).to eq(240)
+        expect([1,2,3,4].new_inject(10) {|memo, item| memo * item}).to eq(240)
+      end
 
-  #   end
+    end
 
-  #   context 'with a symbol argument' do
+    context 'with a symbol argument' do
 
-  #     it 'can add with a starting point and a symbol' do
-  #       expect([1,2,3,4].inject(10, :+)).to eq(20)
-  #       expect([1,2,3,4].new_inject(10, :+)).to eq(20)
-  #     end
+      it 'can add with a starting point and a symbol' do
+        expect([1,2,3,4].inject(10, :+)).to eq(20)
+        expect([1,2,3,4].new_inject(10, :+)).to eq(20)
+      end
 
-    # end
+        it 'can subtract with a starting point and a symbol' do
+        expect([1,2,3,4].inject(:-)).to eq(-8)
+        expect([1,2,3,4].new_inject(:-)).to eq(-8)
+     end
+
+    end
   
 
 end
