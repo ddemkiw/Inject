f74f1b660f22c0bbc4fd3bd87922e3eb96f5df97
Danielle Demkiw <ddemkiw@gmail.com>
2015-01-05 13:06:24 +0000

    Using *args for inject arguments

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,50 +1,38 @@
 class Array
 
-  def new_inject(arg=nil, symbol=nil, &block)
-    return new_inject(&arg) if arg.is_a? Symbol
-    return new_inject(arg, &symbol) if symbol
-    arg ? memo = arg : memo = self.shift
-    self.each { |item| memo = yield memo, item }
-    return memo
-  end
-  
-end
-
-
-#   def new_inject(*args, &block)
-#       copy = self.dup
-      
-#       if args[0].is_a? Symbol
-#         method = args.shift.to_s
-#         sum = eval copy.join method
-#       elsif args[0].is_a? Integer 
-#         if block_given?
-#           sum = args[0]
-#           while copy.length > 0
-#             next_item = copy.shift
-#             sum = yield(sum, next_item) 
-#           end
-#         else 
-#           start = args.shift
-#           method = args.shift.to_s
-#           copy.insert(0,start)
-#           sum =  (eval copy.join method)
-#         end
-#       else args == nil
-#         if block_given?
-#           first_item = copy.shift
-#           second_item = copy.shift
-#           sum = yield(first_item, second_item)   
-#             while copy.length > 0
-#               next_item = copy.shift
-#               sum = yield(sum, next_item) 
-#             end
-#         end
-#       end
-      
-#     sum
+#   def new_inject(arg=nil, symbol=nil, &block)
+#     return new_inject(&arg) if arg.is_a? Symbol
+#     return new_inject(arg, &symbol) if symbol
+#     arg ? memo = arg : memo = self.shift
+#     self.each { |item| memo = yield memo, item }
+#     return memo
 #   end
-
+  
 # end
+
+
+  def new_inject(*args, &block)
+    copy = self.dup
+    return new_inject(&args.shift) if args[0].is_a? Symbol
+    
+    if args[0].is_a? Integer
+        if block_given?
+          memo = args[0]
+          self.each { |item| memo = yield memo, item }
+          return memo
+        else 
+          memo = args[0]
+          return new_inject(memo, &(args[1]))
+        end  
+
+    
+      else
+        memo = self.shift
+        self.each { |item| memo = yield memo, item }
+       return memo
+      end
+    end 
+
+end
 
 
